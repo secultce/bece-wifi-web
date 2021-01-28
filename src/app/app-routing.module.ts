159cdd4a79a0f2f61cb1a7bb8da31694caa12912
WylderM <wyldermenezes@gmail.com>
2021-01-28 20:28:56 -0300

    configuração

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { UserComponent } from './pages/user/user.component';
+import { VisitorComponent } from './pages/visitor/visitor.component';
+import { VoucherComponent } from './pages/voucher/voucher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "",   redirectTo: '/login', pathMatch: 'full'},
+  {path:"login", component: LoginComponent},
+  {path:"forgot-password", component: ForgotPasswordComponent},
+  {path:"users", component: UserComponent},
+  {path:"visitors", component: VisitorComponent},
+  {path:"vouchers", component: VoucherComponent},
+  {path:"**", component: NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { VisitorComponent } from './pages/visitor/visitor.component';
+import { VoucherComponent } from './pages/voucher/voucher.component';
+import { UserComponent } from './pages/user/user.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavBarMenuComponent } from './shared/components/nav-bar-menu/nav-bar-menu.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/angular-material.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      VisitorComponent,
+      VoucherComponent,
+      UserComponent,
+      LoginComponent,
+      NavBarMenuComponent,
+      NotFoundComponent,
+      ForgotPasswordComponent,
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      FlexLayoutModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MaterialModule,
+   ],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
